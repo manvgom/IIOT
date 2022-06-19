d4a615ae3255e1ae8ec5c77963e774af32ffb261
manvgom <72413802+manvgom@users.noreply.github.com>
2022-06-20 01:39:01 +0200

    Update SistemaJson.ino

--- SistemaJson.ino
+++ SistemaJson.ino
@@ -1,5 +1,7 @@
 #include "DHT.h" // Include DHT library
 //#include "HX711.h"
+#include <TinyGPS++.h>
+//#include <SoftwareSerial.h>
 #include <ArduinoJson.h>  // https://arduinojson.org/
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
 #include <WiFi.h>
@@ -41,21 +43,21 @@ void setup() {
 void loop() {
   checkConnections();
           
-  static float t_camion = dhtSensor.readTemperature();
-  static float lat_camion = 42.05606;
-  static float lon_camion = 2.05234;
-  static float h_fabrica = dhtSensor.readHumidity();           
-  static float t_fabrica = dhtSensor.readTemperature();
-  static float h_granja = dhtSensor.readHumidity(); 
-  static float t_granja = dhtSensor.readTemperature();
-  static float tc_granja = dhtSensor.readTemperature();
-  static float h_oficina = dhtSensor.readHumidity();
-  static float t_oficina = dhtSensor.readTemperature();
-  static float t_vaca = dhtSensor.readTemperature();
-  static float lat_vaca = 42.606;
-  static float lon_vaca = 2.234;
-  static float comida = 100;  //scale.get_units(10);
-  static float peso = 540;   //scale.get_units(10);
+  float t_camion = dhtSensor.readTemperature();
+  float lat_camion = 42.05606;
+  float lon_camion = 2.05234;
+  float h_fabrica = dhtSensor.readHumidity();           
+  float t_fabrica = dhtSensor.readTemperature();
+  float h_granja = dhtSensor.readHumidity(); 
+  float t_granja = dhtSensor.readTemperature();
+  float tc_granja = dhtSensor.readTemperature();
+  float h_oficina = dhtSensor.readHumidity();
+  float t_oficina = dhtSensor.readTemperature();
+  float t_vaca = dhtSensor.readTemperature();
+  float lat_vaca = 42.606;
+  float lon_vaca = 2.234;
+  float comida = 100;  //scale.get_units(10);
+  float peso = 540;   //scale.get_units(10);
     
   static int nowTime = millis();
   static int startTime = 0;
@@ -186,6 +188,31 @@ String createTopic(char *topic) {
 String randomBoolean(int x) {
   if(x==0){return "false";}
   else{return "true";}
+}
+
+double distanceBetween(double lat1, double long1, double lat2, double long2)
+{
+  // returns distance in meters between two positions, both specified
+  // as signed decimal-degrees latitude and longitude. Uses great-circle
+  // distance computation for hypothetical sphere of radius 6372795 meters.
+  // Because Earth is no exact sphere, rounding errors may be up to 0.5%.
+  // Courtesy of Maarten Lamers
+  double delta = radians(long1-long2);
+  double sdlong = sin(delta);
+  double cdlong = cos(delta);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+  double slat1 = sin(lat1);
+  double clat1 = cos(lat1);
+  double slat2 = sin(lat2);
+  double clat2 = cos(lat2);
+  delta = (clat1 * slat2) - (slat1 * clat2 * cdlong);
+  delta = sq(delta);
+  delta += sq(clat2 * sdlong);
+  delta = sqrt(delta);
+  double denom = (slat1 * slat2) + (clat1 * clat2 * cdlong);
+  delta = atan2(delta, denom);
+  return delta * 6372795;
 }
 
 //COMUNICACIONES///////////////////////////////////////////////////////////////////////////////////////////////////
